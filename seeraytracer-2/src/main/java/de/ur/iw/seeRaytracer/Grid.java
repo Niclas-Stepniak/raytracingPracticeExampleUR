4b5627ec675ec8dc1be364f957505b0e6fc10aa1
Nic <niclas0409@googlemail.com>
2018-06-21 18:20:40 +0200

    changed the grid method
    Broke my blockade

--- seeraytracer-2/src/main/java/de/ur/iw/seeRaytracer/Grid.java
+++ seeraytracer-2/src/main/java/de/ur/iw/seeRaytracer/Grid.java
@@ -1,41 +1,44 @@
 package de.ur.iw.seeRaytracer;
 
-import java.util.Collection;
 import java.util.Iterator;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import java.util.ArrayList;
 
 public class Grid implements Iterable<Vector3D>{
-  private final Collection<Cube> grid = new ArrayList<>();
+  private final ArrayList<Cube> grid = new ArrayList<>();
 
-  public Grid(double size, AxisAlignedBoundingBox boundingbox) {
+  public Grid(double sideLength, AxisAlignedBoundingBox boundingbox) {
 
     Vector3D boxMin = boundingbox.getMin();
-    Vector3D boxMax = boundingbox.getMax();
 
     Vector3D curCubeMin;
     Vector3D curCubeMax;
-
-    Vector3D getMaxOfCurCube = new Vector3D(size, size, size);
-    Vector3D getNextMinInXdir = new Vector3D(size, 0, 0);
-    Vector3D getNextMinInYdir = new Vector3D(0, size, 0);
-    Vector3D getNextMinInZdir = new Vector3D(0, 0, size);
 
     double xLength = boundingbox.getDimensionsLengths()[0];
     double yLength = boundingbox.getDimensionsLengths()[1];
     double zLength = boundingbox.getDimensionsLengths()[2];
 
-    Cube currentCube = null;
+    xLength /=sideLength;
+    yLength /=sideLength;
+    zLength /=sideLength;
 
-    if (currentCube == null) {
-      curCubeMin = boxMin;
-      curCubeMax = curCubeMin.add(getMaxOfCurCube);
-      currentCube = new Cube(curCubeMin, curCubeMax);
-      grid.add(currentCube);
+    for(int x= 0;x < xLength;x++){
+      for(int y = 0;y < yLength;y++){
+        for (int z = 0; z < zLength;z++){
+          curCubeMin = new Vector3D((boxMin.getX()+(x*sideLength)),(boxMin.getY()+(y*sideLength)),
+              (boxMin.getZ()+(z*sideLength)));
+          curCubeMax = new Vector3D((curCubeMin.getX()+sideLength),(curCubeMin.getY()+sideLength),
+              (curCubeMin.getZ()+sideLength));
+          grid.add(new Cube(curCubeMin,curCubeMax));
+        }
+      }
     }
-
   }
-  
+
+  public ArrayList<Cube> getGrid() {
+    return grid;
+  }
+
   @Override
   public Iterator<Vector3D> iterator() {
     return null;
